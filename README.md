7d7dbb1e63e0e62cbda8838fd3a49985c523c9ff
pacimarco <m.paci1987@gmail.com>
2022-07-04 16:18:22 +0200

    fix errore sottrazione con classi. con numeri funziona

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # js-biglietto-treno
 
 chiedere età utente
+chiedere numero di km da percorrere
+        moltiplicare i km x 0.21euro
 
     se età<18 allora il prezzo è km x 0.21 - 20%
 
-    se età è maggiore uguale a 65 allora il prezzo è km x 0.21 - 65%
+    se età è maggiore uguale a 65 allora il prezzo è km x 0.21 - 40%
 
-chiedere numero di km da percorrere
-        moltiplicare i km x 0.21euro
+    altrimenti il prezzo è 0.21 al km
+
+cercare modo per ridurre le cifre decimali
+
--- js/main.js
+++ js/main.js
@@ -7,14 +7,14 @@ const kmNumero = parseInt(kmPasseggero);
 const costoKm = parseInt(0.21);
 
 const scontoMinorenne = parseInt(0.21 / 100 * 20);
-const scontoOver65 = parseInt(0.21 / 100 * 60);
+const scontoOver65 = parseInt(0.21 / 100 * 40);
 
 if (etaNumero <18) {
-    alert("Il tuo prezzo è " + (kmNumero * (0.21 - scontoMinorenne )) .toFixed(2) + " euro");
+    alert("Il tuo prezzo è " + (kmNumero * (0.21 - (0.21 / 100 * 20) )) .toFixed(2) + " euro");
 }
 
 else if  (etaNumero >= 65) {
-    alert("Il tuo prezzo è " + (kmNumero * (0.21 - scontoOver65)) .toFixed(2) + " euro");
+    alert("Il tuo prezzo è " + (kmNumero * (0.21 - (0.21 / 100 * 40))) .toFixed(2) + " euro");
 }
 
 else { (etaNumero >= 18 && etaNumero <65) 
