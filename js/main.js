d4ef3ae27bc756b9491bd8b9ad109f7c6102e4b1
pacimarco <m.paci1987@gmail.com>
2022-07-04 15:59:41 +0200

    fix operazioni sconto con classi

--- js/main.js
+++ js/main.js
@@ -6,15 +6,15 @@ const kmNumero = parseInt(kmPasseggero);
 
 const costoKm = parseInt(0.21);
 
-const scontoMinorenne = parseInt(0.2);
-const scontoOver65 = parseInt(0.6);
+const scontoMinorenne = parseInt(0.21 / 100 * 20);
+const scontoOver65 = parseInt(0.21 / 100 * 60);
 
 if (etaNumero <18) {
-    alert("Il tuo prezzo è " + (kmNumero * (0.21 - (0.21 / 100 * 20) )) .toFixed(2) + " euro");
+    alert("Il tuo prezzo è " + (kmNumero * (0.21 - scontoMinorenne )) .toFixed(2) + " euro");
 }
 
 else if  (etaNumero >= 65) {
-    alert("Il tuo prezzo è " + (kmNumero * (0.21 - (0.21 / 100 * 60))) .toFixed(2) + " euro");
+    alert("Il tuo prezzo è " + (kmNumero * (0.21 - scontoOver65)) .toFixed(2) + " euro");
 }
 
 else { (etaNumero >= 18 && etaNumero <65) 
