59c03b274e420134ee8346f4c507bace926389e3
pacimarco <m.paci1987@gmail.com>
2022-07-04 16:56:26 +0200

    fix parseInt in 0.21

--- js/main.js
+++ js/main.js
@@ -4,21 +4,20 @@ const etaNumero = parseInt(etaPasseggero);
 const kmPasseggero = prompt("Quanti km vuoi percorrere?");
 const kmNumero = parseInt(kmPasseggero);
 
-const costoKm = parseInt(0.21);
+const costoKm = "0.21";
 
-const scontoMinorenne = parseInt(0.21 / 100 * 20);
-const scontoOver65 = parseInt(0.21 / 100 * 40);
+const scontoMinorenne = (0.21 / 100 * 20);
+const scontoOver65 = (0.21 / 100 * 40);
 
 if (etaNumero <18) {
-    alert("Il tuo prezzo è " + (kmNumero * (0.21 - (0.21 / 100 * 20) )) .toFixed(2) + " euro");
+    alert("Il tuo prezzo è " + (kmNumero * (0.21 - scontoMinorenne )) .toFixed(2) + " euro");
 }
 
 else if  (etaNumero >= 65) {
-    alert("Il tuo prezzo è " + (kmNumero * (0.21 - (0.21 / 100 * 40))) .toFixed(2) + " euro");
+    alert("Il tuo prezzo è " + (kmNumero * (0.21 - scontoOver65)) .toFixed(2) + " euro");
 }
 
 else { (etaNumero >= 18 && etaNumero <65) 
     alert("Il tuo prezzo è " + (kmNumero * 0.21) .toFixed(2) + " euro");
 }
 
-//con la sottrazione con numeri funziona lo sconto, con la classe scontoMinorenne no//
