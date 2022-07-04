deeaada2f7760a8040b7711c8f2df629405b2429
pacimarco <m.paci1987@gmail.com>
2022-07-04 15:54:07 +0200

    uso decimali con .toFixed(2)

--- js/main.js
+++ js/main.js
@@ -10,14 +10,14 @@ const scontoMinorenne = parseInt(0.2);
 const scontoOver65 = parseInt(0.6);
 
 if (etaNumero <18) {
-    alert("Il tuo prezzo è " + (kmNumero * (0.21 - (0.21 / 100 * 20) )) + " euro");
+    alert("Il tuo prezzo è " + (kmNumero * (0.21 - (0.21 / 100 * 20) )) .toFixed(2) + " euro");
 }
 
 else if  (etaNumero >= 65) {
-    alert("Il tuo prezzo è " + (kmNumero * (0.21 - (0.21 / 100 * 60))) + " euro");
+    alert("Il tuo prezzo è " + (kmNumero * (0.21 - (0.21 / 100 * 60))) .toFixed(2) + " euro");
 }
 
 else { (etaNumero >= 18 && etaNumero <65) 
-    alert("Il tuo prezzo è " + (kmNumero * 0.21) + " euro");
+    alert("Il tuo prezzo è " + (kmNumero * 0.21) .toFixed(2) + " euro");
 }
 
